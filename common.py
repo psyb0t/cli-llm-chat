6875583a0b17f89823737f93c2cb393855b1d1fe
bw <ciprian@51k.eu>
2024-07-13 20:38:02 +0300

    refactor

--- common.py
+++ common.py
@@ -1,13 +1,22 @@
+# Constants for chat template names
+CHAT_TEMPLATE_CHATML = "chatml"
+CHAT_TEMPLATE_VICUNA = "vicuna"
+CHAT_TEMPLATE_MISTRAL = "mistral"
+CHAT_TEMPLATE_MISTRAL_FUCKED = "mistral_fucked"
+CHAT_TEMPLATE_LLAMA2 = "llama2"
+CHAT_TEMPLATE_OPENCHAT = "openchat"
+CHAT_TEMPLATE_ZEPHYR = "zephyr"
+
 CHAT_TEMPLATES = {
-    ## CHATML
-    "chatml": """{% for message in messages %}
-{{'<|im_start|>' + message['role'] + '\n' + message['content'] + '<|im_end|>' + '\n'}}
+    CHAT_TEMPLATE_CHATML: """{{ bos_token }}
+{% for message in messages %}
+{{ '<|im_start|>' + message['role'] + '\n' + message['content'] + '<|im_end|>\n' }}
 {% endfor %}
 {% if add_generation_prompt %}
 {{ '<|im_start|>assistant\n' }}
 {% endif %}""",
-    ## VICUNA
-    "vicuna": """{% for message in messages %}
+    CHAT_TEMPLATE_VICUNA: """{{ bos_token }}
+{% for message in messages %}
 {% if message['role'] == 'user' %}
 Human: {{ message['content'] }}
 {% elif message['role'] == 'assistant' %}
@@ -19,12 +28,14 @@ System: {{ message['content'] }}
 {% if add_generation_prompt %}
 Assistant:
 {% endif %}""",
-    ## MISTRAL
-    "mistral": """{% if messages[0]['role'] == 'system' %}
-<s>[INST] {{ messages[0]['content'] }} [/INST]
-</s>
+    CHAT_TEMPLATE_MISTRAL: """{{ bos_token }}
+{% if messages[0]['role'] == 'system' %}
+<s>[INST] {{ messages[0]['content'] }} [/INST]</s>
+{% set loop_messages = messages[1:] %}
+{% else %}
+{% set loop_messages = messages %}
 {% endif %}
-{% for message in messages %}
+{% for message in loop_messages %}
 {% if message['role'] == 'user' %}
 <s>[INST] {{ message['content'] }} [/INST]
 {% elif message['role'] == 'assistant' %}
@@ -32,28 +43,53 @@ Assistant:
 {% endif %}
 {% endfor %}
 {% if add_generation_prompt %}
-<s>[INST]  [/INST]
+<s>[INST] [/INST]
 {% endif %}""",
-    ## MISTRAL FUCKED
-    "mistral_fucked": """{{ bos_token }}{% if messages[0]['role'] == 'system' %}{% if messages[1]['role'] == 'user' %}{{ '[INST] ' + messages[0]['content'] + ' ' + messages[1]['content'] + ' [/INST]' }}{% set loop_messages = messages[2:] %}{% else %}{{ '[INST] ' + messages[0]['content'] + ' [/INST]' }}{% set loop_messages = messages[1:] %}{% endif %}{% else %}{% set loop_messages = messages %}{% endif %}{% for message in loop_messages %}{% if message['role'] == 'user' %}{{ '[INST] ' + message['content'] + ' [/INST]' }}{% elif message['role'] == 'assistant' %}{{ message['content'] + eos_token }}{% else %}{{ raise_exception('Only user and assistant roles are supported!') }}{% endif %}{% endfor %}""",
-    ## LLAMA
-    "llama2": """{% for message in messages %}
-{% if loop.index == 1 and message['role'] == 'system' %}
-<s>[INST] <<SYS>>
-{{ message['content'] }}
-<</SYS>>
-
-{% elif message['role'] == 'user' %}
-{{ message['content'] }} [/INST]
+    CHAT_TEMPLATE_MISTRAL_FUCKED: """{{ bos_token }}
+{% if messages[0]['role'] == 'system' %}
+{% if messages[1]['role'] == 'user' %}
+{{ '[INST] ' + messages[0]['content'] + ' ' + messages[1]['content'] + ' [/INST]' }}
+{% set loop_messages = messages[2:] %}
+{% else %}
+{{ '[INST] ' + messages[0]['content'] + ' [/INST]' }}
+{% set loop_messages = messages[1:] %}
+{% endif %}
+{% else %}
+{% set loop_messages = messages %}
+{% endif %}
+{% for message in loop_messages %}
+{% if message['role'] == 'user' %}
+{{ '[INST] ' + message['content'] + ' [/INST]' }}
 {% elif message['role'] == 'assistant' %}
-{{ message['content'] }} </s><s>[INST]
+{{ message['content'] + eos_token }}
+{% else %}
+{{ raise_exception('Only user and assistant roles are supported!') }}
+{% endif %}
+{% endfor %}""",
+    CHAT_TEMPLATE_LLAMA2: """{{ bos_token }}
+{% if messages[0]['role'] == 'system' %}
+<s>[INST] <<SYS>>
+{{ messages[0]['content'] }}
+<</SYS>>
+{% set loop_messages = messages[1:] %}
+{% else %}
+{% set loop_messages = messages %}
+{% endif %}
+{% for message in loop_messages %}
+{% if message['role'] == 'user' %}
+{% if not loop.first %}</s><s>{% endif %}[INST] {{ message['content'] }} [/INST]
+{% elif message['role'] == 'assistant' %}
+{{ message['content'] }}
 {% endif %}
 {% endfor %}
 {% if add_generation_prompt %}
-[/INST]
+{% if messages[-1]['role'] == 'user' %}
+{% else %}
+</s><s>[INST] [/INST]
+{% endif %}
 {% endif %}""",
-    ## OPENCHAT
-    "openchat": """{% for message in messages %}
+    CHAT_TEMPLATE_OPENCHAT: """{{ bos_token }}
+{% for message in messages %}
 {% if message['role'] == 'user' %}
 Human: {{ message['content'] }}
 {% elif message['role'] == 'assistant' %}
@@ -65,9 +101,9 @@ System: {{ message['content'] }}
 {% if add_generation_prompt %}
 Assistant:
 {% endif %}""",
-    ## ZEPHYR
-    "zephyr": """{% for message in messages %}
-{% if loop.index == 1 and message['role'] == 'system' %}
+    CHAT_TEMPLATE_ZEPHYR: """{{ bos_token }}
+{% for message in messages %}
+{% if loop.first and message['role'] == 'system' %}
 <|system|>
 {{ message['content'] }}
 {% elif message['role'] == 'user' %}
